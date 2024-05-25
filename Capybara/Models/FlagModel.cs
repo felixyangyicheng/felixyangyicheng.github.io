205e6755b3c49b6111710679b4050ab95f9c9bb2
felixyangyicheng <admin-fr06ab@ermo-group.tosb>
2024-05-25 17:17:21 +0200

    guessflag random pick

---
+++ Capybara/Models/FlagModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Capybara.Models
+{
+    public class FlagModel
+    {
+        public string Region { get; set; } = "";
+        public List<string> Official { get; set; }= new List<string>();
+        public List<string> Proposed { get; set; } = new List<string>();
+        public List<string> Details { get; set; } = new List<string>();
+    }
+}
---
+++ Capybara/Pages/GuessFlags/GuessFlag.razor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Text.Json;
+
+namespace Capybara.Pages.GuessFlags
+{
+    public partial class GuessFlag
+    {
+        [NotNull]
+        public List<FlagModel>? regionAndFlags { get; set; } = new();
+        [NotNull]
+
+        public FlagModel RegionToGuess { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            regionAndFlags=JsonSerializer.Deserialize<List<FlagModel>>(File.ReadAllText("./place_flags.json"));
+            Random rnd = new Random();
+            if (regionAndFlags!=null)
+            {
+                RegionToGuess = regionAndFlags[rnd.Next(regionAndFlags.Count)];
+            }
+            await base.OnInitializedAsync();
+        }
+    }
+}
