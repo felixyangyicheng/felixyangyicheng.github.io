4d45749826c1d18610e681377d1ff4a2bd4b2d7f
felixyangyicheng <50945409+felixyangyicheng@users.noreply.github.com>
2024-06-07 14:01:07 +0000

    Deploying to gh-pages from @ felixyangyicheng/felixyangyicheng.github.io@e5dc5daac667a9a8e8a12e7ed35cdaf653073d68 🚀

--- README.md
+++
@@ -1,2 +0,0 @@
- ## Licence 
-[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/) 
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-/* Manifest version: +SS9qs7l */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
@@ -66,3 +65,4 @@ self.addEventListener('notificationclick', event => {
     event.notification.close();
     event.waitUntil(clients.openWindow(event.notification.data.url));
 });
+/* Manifest version: v7zgdnzl */
