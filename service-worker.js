c221e05014b19ab434594a6e7f24cee4f5caeaab
felixyangyicheng <admin-fr06ab@ermo-group.tosb>
2024-06-07 09:09:36 +0200

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+/* Manifest version: LgRZrtsg */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
@@ -65,4 +66,3 @@ self.addEventListener('notificationclick', event => {
     event.notification.close();
     event.waitUntil(clients.openWindow(event.notification.data.url));
 });
-/* Manifest version: DdGR1KbT */
