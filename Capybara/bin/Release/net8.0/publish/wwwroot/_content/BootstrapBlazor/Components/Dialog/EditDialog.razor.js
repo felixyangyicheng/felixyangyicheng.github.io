fc321da421990d31855bb253e7cfac1901543582
felixyangyicheng <50945409+felixyangyicheng@users.noreply.github.com>
2024-07-09 12:23:48 +0200

    Delete Capybara/bin/Release/net8.0 directory

--- Capybara/bin/Release/net8.0/publish/wwwroot/_content/BootstrapBlazor/Components/Dialog/EditDialog.razor.js
+++
@@ -1,9 +0,0 @@
-﻿export function execute(id, show) {
-    const el = document.getElementById(id)
-    if (show) {
-        el.classList.add('show')
-    }
-    else {
-        el.classList.remove('show')
-    }
-}
