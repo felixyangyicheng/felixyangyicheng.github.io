04d34d2d86fad87763fb8a18b0995ee12336c16a
felixyangyicheng <50945409+felixyangyicheng@users.noreply.github.com>
2024-08-16 17:09:07 +0200

    domino svg

---
+++ Capybara/Components/Dominos/Piece.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace Capybara.Components.Dominos
+{
+    public partial class Piece
+    {
+        /// <summary>
+        /// Domino value to dispatch
+        /// </summary>
+        [Parameter]
+        public Domino Domino { get; set; } = default!;
+        /// <summary>
+        /// View need to be rotated ?
+        /// </summary>
+        [Parameter]
+        public bool Rotate { get; set; }
+
+        protected override void OnInitialized()
+        {
+
+        }
+    }
+}
