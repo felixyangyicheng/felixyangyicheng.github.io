feabf9e4fb1042eeed4eb48b9964d79f4b8ac753
felixyangyicheng <50945409+felixyangyicheng@users.noreply.github.com>
2024-08-17 17:49:57 +0200

    piece css style init

--- Capybara/Components/Dominos/Piece.razor.cs
+++ Capybara/Components/Dominos/Piece.razor.cs
@@ -13,11 +13,34 @@
         [Parameter]
         public bool Rotate { get; set; }
 
-
+        /// <summary>
+        /// Complementary css class name
+        /// </summary>
+        [Parameter] 
+        public string Class { get; set; } = "";
+        /// <summary>
+        /// BasePath to get raw content
+        /// </summary>
         public string BasePath { get; set; } = "";
         protected override void OnInitialized()
         {
             BasePath= $"{Configuration["raw.githubusercontent"]}";
         }
+
+        private string DominoImage(int number) => number switch
+        {
+            0  => $"{BasePath}/img/dominos/domino0.svg",
+            1 => $"{BasePath}/img/dominos/domino1.svg",
+            2 => $"{BasePath}/img/dominos/domino2.svg",
+            3 => $"{BasePath}/img/dominos/domino3.svg",
+            4 => $"{BasePath}/img/dominos/domino4.svg",
+            5 => $"{BasePath}/img/dominos/domino5.svg",
+            6 => $"{BasePath}/img/dominos/domino6.svg",
+            7 => $"{BasePath}/img/dominos/domino7.svg",
+            8 => $"{BasePath}/img/dominos/domino8.svg",
+            9 => $"{BasePath}/img/dominos/domino9.svg",
+
+            _ => throw new ArgumentOutOfRangeException(nameof(number), $"Not expected direction value: {number}"),
+        };
     }
 }
