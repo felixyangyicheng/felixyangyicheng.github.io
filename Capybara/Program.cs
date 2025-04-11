7b9c48291c8efcda6544483c441a2880ef109c5d
felixyangyicheng <felixyangyicheng@gmail.com>
2025-04-11 17:30:10 +0100

    docs components

--- Capybara/Program.cs
+++ Capybara/Program.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+
 using Capybara.HashCheckService;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
