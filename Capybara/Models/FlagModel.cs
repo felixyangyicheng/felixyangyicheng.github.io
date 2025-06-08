3159c0ca32abeace89971a5101b4d0fbc8f8aa1c
felixyangyicheng <felix.yang.yicheng@gmail.com>
2025-06-08 15:19:09 +0200

    country quiz

--- Capybara/Models/FlagModel.cs
+++ Capybara/Models/FlagModel.cs
@@ -3,6 +3,7 @@
     public class FlagModel
     {
         public string Region { get; set; } = "";
+        public string Code { get; set; } = "";
         public List<string> Official { get; set; }= new List<string>();
         public List<string> Proposed { get; set; } = new List<string>();
         public List<string> Details { get; set; } = new List<string>();
